710a41e2d3fa25eca0f2398281d9d66ca27ac800
HuangHao's MacBook Air <ytphuanghao@foxmail.com>
2025-08-03 15:56:25 +0800

    添加test1文件

---
+++ Test_folder/test1.py
@@ -0,0 +1,5 @@
+print("Hello, World!")
+# This script is designed to be run directly or imported as a module.
+# If run directly, it will execute the main functions.
+# If imported, it will not execute the main functions.
+# The main functions are say_hello and say_goodbye.
